53caed4cb0cbf65db7a010796952cf694c7e6e84
SM <turr3lim@sudomail.com>
2023-11-04 16:31:31 -0600

    Edits to views dot py

--- ducky/fowl/views.py
+++ ducky/fowl/views.py
@@ -1,15 +1,30 @@
 
 from django.shortcuts import render
 from .forms import DuckEntry
+from django.http import HttpResponse
 
 # Create your views here.
 
 def frontpage(request):
     return render(request, "fowl/frontpage.html")
 
+
+def editor(request):
+    if request.method == 'POST':
+        form = DuckEntry(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data["my_form_field_name"])
+            return HttpResponse("I made a request")
+    else:
+        form = DuckEntry()
+    return render(request, 'fowl/editor.html', {"form":form})
+
+
+'''
 def editor(request):
     context = {}
     form = DuckEntry(request.POST)
     context['form'] = form
     print(form)
-    return render(request, 'fowl/editor.html', context)
+    return render(request, 'fowl/editor.html', {'context':context})
+'''
