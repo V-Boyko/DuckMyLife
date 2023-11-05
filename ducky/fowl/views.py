e13e10dded8a081115192199310805d8a57237af
SM <turr3lim@sudomail.com>
2023-11-04 18:51:04 -0600

    Message captured as string

--- ducky/fowl/views.py
+++ ducky/fowl/views.py
@@ -10,15 +10,22 @@ def frontpage(request):
 
 
 def editor(request):
+   form = DuckEntry(request.POST)
+   text = request.POST.get('text-box', "")
+   print(type(text))
+   return render(request, 'fowl/editor.html') 
+
+'''
+def editor(request):
     if request.method == 'POST':
         form = DuckEntry(request.POST)
         if form.is_valid():
-            print(form.cleaned_data["my_form_field_name"])
+            print(request.get)
             return HttpResponse("I made a request")
     else:
         form = DuckEntry()
     return render(request, 'fowl/editor.html', {"form":form})
-
+'''
 
 '''
 def editor(request):
